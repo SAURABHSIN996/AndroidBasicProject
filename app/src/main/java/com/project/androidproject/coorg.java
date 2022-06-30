65aa90e479adec30ca8b03a5330180962d09dfce
saurabh singh <saurabh.9901s@gmail.com>
2022-06-30 22:50:50 +0530

    all blog page published

---
+++ app/src/main/java/com/project/androidproject/coorg.java
@@ -0,0 +1,14 @@
+package com.project.androidproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class coorg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coorg);
+    }
+}
--- app/src/main/java/com/project/androidproject/nandiHills.java
+++ app/src/main/java/com/project/androidproject/nandiHills.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class blogPage extends AppCompatActivity {
+public class nandiHills extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blog_page);
+        setContentView(R.layout.activity_nandi_hills);
     }
 }
