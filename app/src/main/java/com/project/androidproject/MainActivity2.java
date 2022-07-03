360712d5dc243115a22d9cfb93f9cba1fdb2cd8f
saurabh singh <saurabh.9901s@gmail.com>
2022-07-04 01:06:26 +0530

    all pages linked together and working

--- app/src/main/java/com/project/androidproject/MainActivity2.java
+++ app/src/main/java/com/project/androidproject/MainActivity2.java
@@ -11,73 +11,84 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    private void startMyActivity(Intent intent) {
+        startActivity(intent);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+
         MaterialButton nandibtn = (MaterialButton) findViewById(R.id.nandibtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,nandiHills.class));
-            }
-        });
-
         MaterialButton chikmangalurbtn = (MaterialButton) findViewById(R.id.chikmangalurbtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,chikmangalur.class));
-            }
-        });
-
         MaterialButton hampibtn = (MaterialButton) findViewById(R.id.hampibtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,hampi.class));
-            }
-        });
-
         MaterialButton coorgbtn = (MaterialButton) findViewById(R.id.coorgbtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,coorg.class));
-            }
-        });
-
         MaterialButton mangalorebtn = (MaterialButton) findViewById(R.id.mangalorebtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,mangalore.class));
-            }
-        });
-
         MaterialButton jogFallsbtn = (MaterialButton) findViewById(R.id.jogFallsbtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,jogFalls.class));
-            }
-        });
-
         MaterialButton udupibtn = (MaterialButton) findViewById(R.id.udupibtn);
+        MaterialButton gokarnabtn = (MaterialButton) findViewById(R.id.gokarnabtn);
+
+
         nandibtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,udupi.class));
+                Intent int1 = new Intent(MainActivity2.this, nandiHills.class);
+                startActivity(int1);
             }
         });
 
-        MaterialButton gokarnabtn = (MaterialButton) findViewById(R.id.gokarnabtn);
-        nandibtn.setOnClickListener(new View.OnClickListener() {
+        chikmangalurbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity2.this,gokarna.class));
+                Intent int2 = new Intent(MainActivity2.this, chikmangalur.class);
+                startActivity(int2);
             }
+        });
+        
+        hampibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int3 = new Intent(MainActivity2.this, hampi.class);
+                startActivity(int3);
+            }
+        });
+
+        coorgbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int4 = new Intent(MainActivity2.this, coorg.class);
+                startActivity(int4);
+            }
+        });
+
+        mangalorebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int5 = new Intent(MainActivity2.this, mangalore.class);
+                startActivity(int5);            }
+        });
+
+        jogFallsbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int6 = new Intent(MainActivity2.this, jogFalls.class);
+                startActivity(int6);            }
+        });
+
+        udupibtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int7 = new Intent(MainActivity2.this, udupi.class);
+                startActivity(int7);            }
+        });
+
+        gokarnabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int8 = new Intent(MainActivity2.this, gokarna.class);
+                startActivity(int8);            }
         });
     }
 }
